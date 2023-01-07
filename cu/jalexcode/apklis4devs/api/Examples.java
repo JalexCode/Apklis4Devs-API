b724ac915bab92e65763e0cfce1f3055eef92dd5
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-01-07 01:08:27 -0500

    Refactor New example methods was added

--- cu/jalexcode/apklis4devs/api/Examples.java
+++ cu/jalexcode/apklis4devs/api/Examples.java
@@ -1,80 +1,106 @@
 package cu.jalexcode.apklis4devs.api;
 
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
-import cu.jalexcode.apklis4devs.api.model.Application;
-import cu.jalexcode.apklis4devs.api.model.Notification;
+import cu.jalexcode.apklis4devs.api.model.*;
+import cu.jalexcode.apklis4devs.api.utils.Constants;
 
 /**
  * Clase que muestra ejemplos de uso de esta api, recuerda que casi toda esta api es experimental asi que algunas partes pueden funcionar de forma inesperada, Disfrutalo!
+ * user = "Tu usuario",
+ *             pass = "Tu contrasenna",
  */
 public class Examples {
-    static String user = "Tu usuario",
-                pass = "Tu contrasenna";
+    static String user = "JavyIsCool01",
+            pass = "Joker991030*",
+            dateFormmat = "yyyy-MM-dd HH:mm:ss",
+            initDate = "2022-01-01 00:00:00",
+            finalDate = "2023-01-01 00:00:00";
+
     /**
      * Ejemplo de uso del login
      */
-    public static final void simpleLogin() throws Exception{
-        Scanner sc = new Scanner(System.in);
-        String user = null,
-                pass = null;
-
-        System.out.print("Introduzca su nombre de usuario: ");
-        user=sc.nextLine();
-        System.out.print("Introduzca su contraseña: ");
-        pass=sc.nextLine();
-
-        try{
-            Session s = new Session(user, pass);
+    public static final Session simpleLogin() throws Exception {
+        Session s = null;
+        try {
+            s = new Session(user, pass);
             //ALERTA: en android necesitaras llamar este metodo en un hilo aparte, porque
             //realiza tareas de red y obtendrias un: NetworkOnMainThreadException
             s.updateCache();
-            System.out.println("Hola "+s.getUserInfo().getName()+"!");
-        }catch(Throwable e){
-            System.err.println("Error accediendo: "+e);
+            System.out.println("Hola " + s.getUserInfo().getName() + "!");
+        } catch (Throwable e) {
+            System.err.println("Error accediendo: " + e);
         }
+        return s;
     }
 
     /**
      * Ejemplo de uso de obtener tus apps
      */
-    public static final void getApps(){
+    public static final void getApps() {
 
-        try{
-            Session s = new Session(user, pass);
-            //ALERTA: en android necesitaras llamar este metodo en un hilo aparte, porque
-            //realiza tareas de red y obtendrias un: NetworkOnMainThreadException
-            s.updateCache();
-            System.out.println("Buscando tus apps, "+s.getUserInfo().getName()+"...");
+        try {
+            Session s = simpleLogin();
             int pos = 0;
-            for (Application app:
-                s.getOwnApps()){
-                System.out.println("App #"+(++pos)+": "+app.getName()+", con un promedio de "+app.getRating()+" estrellas");
+            for (Application app :
+                    s.getOwnApps()) {
+                System.out.println("App #" + (++pos) + ": " + app.getName() + ", con un promedio de " + app.getRating() + " estrellas");
             }
-        }catch(Throwable e){
-            System.err.println("Error accediendo: "+e);
+        } catch (Throwable e) {
+            System.err.println("Error accediendo: " + e);
+        }
+    }
+
+    public static void getNotifications() throws Exception {
+        Session s = simpleLogin();
+        for (Notification n : s.getNotifications()) {
+            System.out.println("Título: " + n.getName());
+            System.out.println("Mensaje: " + n.getMessage());
+        }
+    }
+
+    public static void getBuyers() throws Exception {
+        Session s = simpleLogin();
+        Date date1 = new SimpleDateFormat(dateFormmat).parse(finalDate);
+        Date date2 = new SimpleDateFormat(dateFormmat).parse(initDate);
+        for (ReportResult report :
+                s.getSalesReport(date1, date2, 10, s.getUserInfo().getPhone())) {
+            System.out.println("Comprador: " + report.getBuyer());
+            System.out.println("Telef. Comprador: " + report.getBuyerPhone());
+        }
+    }
+
+    public static void getDashboardStats() throws Exception {
+        // No es necesario introducir credenciales para obtener los datos globales
+        Session s = new Session();
+        DashboardStats dashboardStats = s.getDashboardStats(true);
+        System.out.println("Aplicaciones cubanas: " + dashboardStats.getCuban_apps());
+    }
+
+    public static void getUserInfo() throws Exception {
+        Session s = simpleLogin();
+        UserInfo userInfo = s.getUserInfo();
+        System.out.println("Nombre completo: " + userInfo.getFullName());
+    }
+
+    public static void getSales() throws Exception {
+        Session s = simpleLogin();
+        //
+        Date date1 = new SimpleDateFormat(dateFormmat).parse(finalDate);
+        Date date2 = new SimpleDateFormat(dateFormmat).parse(initDate);
+        //
+        ArrayList<Sale> saleArrayList = s.getSales(date2, date1, s.getUserInfo().getPhone(), Constants.TYPE_DAY);
+        for (Sale sale : saleArrayList){
+            System.out.println("===============================");
+            System.out.println("Fecha: " + new SimpleDateFormat(dateFormmat).format(sale.getDate()));
+            System.out.println("Ventas: " + sale.getSales());
+            System.out.println("Dinero recaudado: " + sale.getAmmount());
         }
     }
 
     public static void main(String[] args) throws Exception {
-        try{
-            Session s = new Session(user, pass);
-            //ALERTA: en android necesitaras llamar este metodo en un hilo aparte, porque
-            //realiza tareas de red y obtendrias un: NetworkOnMainThreadException
-            s.updateCache();
-//            Date date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2023-01-04 16:30:00");
-//            Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2022-11-01 00:30:00");
-//            for (ReportResult report:
-//                    s.getSalesReport(date1, date2, 10, s.getUserInfo().getPhone())){
-//                System.out.println(report.getBuyer());
-//            }
-//            UserInfo userInfo = s.resolveAndGetUserInfo();
-            for (Notification n:s.getNotifications()){
-                System.out.println(n.getMessage());
-            }
-
-        }catch(Throwable e){
-            System.err.println("Error accediendo: "+e);
-        }
+        getSales();
     }
 }

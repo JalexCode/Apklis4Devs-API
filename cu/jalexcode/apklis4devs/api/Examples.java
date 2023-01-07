7f5a6619173baef3aedf624964ba91946cb395d4
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-01-07 01:23:07 -0500

    Refactor New example methods was added

--- cu/jalexcode/apklis4devs/api/Examples.java
+++ cu/jalexcode/apklis4devs/api/Examples.java
@@ -9,12 +9,10 @@ import cu.jalexcode.apklis4devs.api.utils.Constants;
 
 /**
  * Clase que muestra ejemplos de uso de esta api, recuerda que casi toda esta api es experimental asi que algunas partes pueden funcionar de forma inesperada, Disfrutalo!
- * user = "Tu usuario",
- *             pass = "Tu contrasenna",
  */
 public class Examples {
-    static String user = "JavyIsCool01",
-            pass = "Joker991030*",
+    static String user = "TU USUARIO",
+            pass = "TU CONTRASENNA",
             dateFormmat = "yyyy-MM-dd HH:mm:ss",
             initDate = "2022-01-01 00:00:00",
             finalDate = "2023-01-01 00:00:00";
@@ -25,11 +23,12 @@ public class Examples {
     public static final Session simpleLogin() throws Exception {
         Session s = null;
         try {
+            System.out.println("===== Iniciando sesión... =====");
             s = new Session(user, pass);
             //ALERTA: en android necesitaras llamar este metodo en un hilo aparte, porque
             //realiza tareas de red y obtendrias un: NetworkOnMainThreadException
             s.updateCache();
-            System.out.println("Hola " + s.getUserInfo().getName() + "!");
+            System.out.println("Hola " + s.getUserInfo().getName() + "[" + s.getUserInfo().getUser() + "]" + "!");
         } catch (Throwable e) {
             System.err.println("Error accediendo: " + e);
         }
@@ -40,13 +39,17 @@ public class Examples {
      * Ejemplo de uso de obtener tus apps
      */
     public static final void getApps() {
-
         try {
             Session s = simpleLogin();
+            System.out.println("= APLICACIONES =");
             int pos = 0;
             for (Application app :
                     s.getOwnApps()) {
-                System.out.println("App #" + (++pos) + ": " + app.getName() + ", con un promedio de " + app.getRating() + " estrellas");
+                System.out.println("===============================");
+                System.out.println("App #" + (++pos));
+                System.out.println("Nombre: " + app.getName());
+                System.out.println("Clasificación: " + app.getRating() + " estrellas");
+                System.out.println("Cantidad de descargas: " + app.getDownloadCount());
             }
         } catch (Throwable e) {
             System.err.println("Error accediendo: " + e);
@@ -55,34 +58,47 @@ public class Examples {
 
     public static void getNotifications() throws Exception {
         Session s = simpleLogin();
+        System.out.println("= NOTIFICACIONES =");
         for (Notification n : s.getNotifications()) {
+            System.out.println("===============================");
             System.out.println("Título: " + n.getName());
             System.out.println("Mensaje: " + n.getMessage());
         }
     }
 
-    public static void getBuyers() throws Exception {
+    public static void getSalesReport() throws Exception {
         Session s = simpleLogin();
         Date date1 = new SimpleDateFormat(dateFormmat).parse(finalDate);
         Date date2 = new SimpleDateFormat(dateFormmat).parse(initDate);
+        System.out.println("= REPORTE DE VENTAS =");
         for (ReportResult report :
                 s.getSalesReport(date1, date2, 10, s.getUserInfo().getPhone())) {
+            System.out.println("===============================");
             System.out.println("Comprador: " + report.getBuyer());
             System.out.println("Telef. Comprador: " + report.getBuyerPhone());
+            System.out.println("Cantidad pagada: " + report.getAmmount());
+            System.out.println("Estado: " + report.getState());
         }
     }
 
     public static void getDashboardStats() throws Exception {
         // No es necesario introducir credenciales para obtener los datos globales
         Session s = new Session();
+        System.out.println("= ESTADISTICAS GLOBALES =");
         DashboardStats dashboardStats = s.getDashboardStats(true);
         System.out.println("Aplicaciones cubanas: " + dashboardStats.getCuban_apps());
+        System.out.println("Cantidad de desarrolladores: " + dashboardStats.getDevelopers());
+        System.out.println("Aplicaciones de pago: " + dashboardStats.getPaid_apps());
+        System.out.println("Total de aplicaciones: " + dashboardStats.getApp());
+        System.out.println("Cantidad de usuarios: " + dashboardStats.getUsers());
     }
 
     public static void getUserInfo() throws Exception {
         Session s = simpleLogin();
+        System.out.println("= INFORMACION DEL USUARIO =");
         UserInfo userInfo = s.getUserInfo();
         System.out.println("Nombre completo: " + userInfo.getFullName());
+        System.out.println("Cuenta de banco: " + userInfo.getSellerBankAccount());
     }
 
     public static void getSales() throws Exception {
@@ -91,6 +107,7 @@ public class Examples {
         Date date1 = new SimpleDateFormat(dateFormmat).parse(finalDate);
         Date date2 = new SimpleDateFormat(dateFormmat).parse(initDate);
         //
+        System.out.println("= VENTAS =");
         ArrayList<Sale> saleArrayList = s.getSales(date2, date1, s.getUserInfo().getPhone(), Constants.TYPE_DAY);
         for (Sale sale : saleArrayList){
             System.out.println("===============================");
@@ -101,6 +118,17 @@ public class Examples {
     }
 
     public static void main(String[] args) throws Exception {
+        // obtener las aplicaciones publicadas por ti
+        getApps();
+        // obtener tu informacion de usuario
+        getUserInfo();
+        // obtener ventas en un rang de fecha
         getSales();
+        // obtener reporte de ventas
+        getSalesReport();
+        // obtener notificaciones
+        getNotifications();
+        // obtener datos globales de la plataforma
+        getDashboardStats();
     }
 }
